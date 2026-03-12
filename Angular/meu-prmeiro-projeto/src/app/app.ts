89b5c8a363b5b55a08322f03e7a1776adef20c7f
Guilherme Zermiani <guilherme.zermianitec@gmail.com>
2026-03-11 21:55:07 -0300

    Testes Angular

---
+++ Angular/meu-prmeiro-projeto/src/app/app.ts
@@ -0,0 +1,21 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, FormsModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  title:string = ('Curso Técnico');
+
+  contador: number = 0;
+  incrementar(){
+    this.contador++;
+  }
+  textoDigitado: string = '';
+}
+
+  
